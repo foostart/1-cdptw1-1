fc43b7d4e1270614663130bd006c8a5b3e43817e
foostart.com <foostart.com@gmail.com>
2019-09-12 13:38:04 +0700

    fix bug

---
+++ 995/995.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/995.less', 'css/995.css');
+?>
+<html lang="vi">
+    <head>      
+        <title>995</title>
+        <meta charset="utf-8">           
+        <link href="<?php echo $url_path ?>/css/all_css.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/995.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php include $dir_block . '/995-content.php'; ?>
+    </body>
+
+</html>
