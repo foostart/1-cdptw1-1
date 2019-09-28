892fdd259e499b0a48e80f6dc59f907eea1d4ebe
AnhAnh99 <1999buianh@gmail.com>
2019-09-28 20:15:45 +0700

    ok

--- 1801/1801.php
+++ 1801/1801.php
@@ -20,7 +20,7 @@
 
 <html lang='en'>
     <head>
-        <title>module 1801</title>
+        <title>1801</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
