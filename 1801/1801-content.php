7f10d9de4fff15dd4ebf5d07916197836d238aef
AnhAnh99 <1999buianh@gmail.com>
2019-09-28 21:45:53 +0700

    ok

--- 1801/1801-content.php
+++ 1801/1801-content.php
@@ -7,70 +7,47 @@ preg_match_all($pattern_uri, __DIR__, $matches);
 $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-
-   <div class="type-1801">
-      <div class="main-container">
-         <div id="carousel-example-generic" class="carousel slide carousel-fade">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-               <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
-               <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-               <li data-target="#carousel-example-generic" data-slide-to="2" class="active"></li>
-            </ol>
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-               <!-- First slide -->
-               <div class="item slide-1 text-left">
-                  <div class="carousel-caption">
-                     <p data-animation="animated fadeInUp">
-                        Featured Project
-                     </p>
-                     <h3 data-animation="animated fadeInUp">
-                        Green Interior
-                     </h3>
-                     <a href="contact.html" class="btn btn-primary btn-lg fadeInUp" data-animation="animated zoomIn">
-                        Our Portfolio</a>
-                  </div>
-               </div>
-               <!-- /.item -->
-               <!-- Second slide -->
-               <div class="item slide-2 text-left">
-                  <div class="carousel-caption">
-                     <p data-animation="animated fadeInUp">
-                        Featured Project
-                     </p>
-                     <h3 data-animation="animated fadeInUp">
-                        Green Interior
-                     </h3>
-                     <a href="#" class="btn btn-primary btn-lg fadeInUp" data-animation="animated zoomIn">Our Portfolio</a>
-                  </div>
-               </div>
-               <!-- /.item -->
-               <!-- Third slide -->
-               <div class="item slide-3 text-left active">
-                  <div class="carousel-caption">
-                     <p data-animation="animated fadeInUp">
-                        Featured Project
-                     </p>
-                     <h3 data-animation="animated fadeInUp">
-                        Green Interior
-                     </h3>
-                     <a href="#" class="btn btn-primary btn-lg fadeInUp" data-animation="animated zoomIn">Our Portfolio</a>
-                  </div>
-               </div>
-               <!-- /.item -->
-            </div>
-            <!-- /.carousel-inner -->
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-               <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-               <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-               <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-               <span class="sr-only">Next</span>
-            </a>
-         </div>
-         <!-- /.carousel -->
+<!-- Swiper -->
+<div class="type-1801 swiper-container">
+   <div class="swiper-wrapper">
+      <div class="swiper-slide">
+         <p data-animation="animated fadeInUp">
+            Featured Project
+         </p><br>
+         <h3 data-animation="animated fadeInUp">
+            Green Interior
+         </h3><br>
+         <a href="#" class="btn btn-primary btn-lg fadeInUp" data-animation="animated zoomIn">
+            Our Portfolio
+         </a>
       </div>
+      <div class="swiper-slide">
+      <p data-animation="animated fadeInUp">
+            Featured Project
+         </p><br>
+         <h3 data-animation="animated fadeInUp">
+            Green Interior
+         </h3><br>
+         <a href="#" class="btn btn-primary btn-lg fadeInUp" data-animation="animated zoomIn">
+            Our Portfolio
+         </a>
+      </div>
+      <div class="swiper-slide">
+      <p data-animation="animated fadeInUp">
+            Featured Project
+         </p><br>
+         <h3 data-animation="animated fadeInUp">
+            Green Interior
+         </h3><br>
+         <a href="#" class="btn btn-primary btn-lg fadeInUp" data-animation="animated zoomIn">
+            Our Portfolio
+         </a>
+      </div>
+
    </div>
+   <!-- Add Pagination -->
+   <div class="swiper-pagination"></div>
+   <!-- Add Arrows -->
+   <div class="swiper-button-next"></div>
+   <div class="swiper-button-prev"></div>
+</div>
